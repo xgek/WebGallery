f3d6fe3df69964cd48d3a337c0db2dd57071dd14
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:13:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/web3gallery/main.go
@@ -0,0 +1,23 @@
+// cmd/web3gallery/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"web3gallery/internal/web3gallery"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := web3gallery.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
